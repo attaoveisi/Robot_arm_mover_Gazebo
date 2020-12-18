570d821e4cbaf4f60e63beab9c13fe3120d81020
ATTA OVEISI <atta.oveisi@torc.ai>
2020-12-18 17:07:09 +0100

    stable commit

--- workspace/catkin_ws/runRobot.sh
+++ workspace/catkin_ws/runRobot.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-catkin_make
-source devel/setup.bash
-roslaunch simple_arm robot_spawn.launch
-sleep 10 
-rosrun simple_arm simple_mover
+catkin_make && sleep 2
+source devel/setup.bash && sleep 2
+
+roslaunch simple_arm robot_spawn.launch && sleep 2
+
+#rosrun simple_arm simple_mover
+
